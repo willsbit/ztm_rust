f49d389dc7b54584a3ea0e3263170483189f6975
Willian <willsbit@gmail.com>
2022-03-11 15:00:21 -0300

    Data types: struct

--- rust-course-data/exercises/activities/src/bin/a8.rs
+++ rust-course-data/exercises/activities/src/bin/a8.rs
@@ -8,5 +8,33 @@
 // * Use a struct to store drink flavor and fluid ounce information
 // * Use a function to print out the drink flavor and ounces
 // * Use a match expression to print the drink flavor
+#[derive(Debug)]
+enum DrinkFlavors {
+    Cola,
+    Lemon,
+    Strawberry,
+    Pineapple
+}
 
-fn main() {}
+struct Drink{
+    flavor:DrinkFlavors,
+    fl_oz: f64
+}
+
+fn show_drink(drink: Drink) {
+    println!("Fluid ounces: {:?}", drink.fl_oz);
+    match drink.flavor {
+        DrinkFlavors::Cola => {println!("It's a cola flavored drink!")}
+        DrinkFlavors::Lemon => {println!("It's a lemon flavored drink!")}
+        DrinkFlavors::Strawberry => {println!("It's a strawberry flavored drink!")}
+        DrinkFlavors::Pineapple => {println!("It's a pineapple flavored drink!")}
+    }
+}
+
+fn main() {
+    let my_drink = Drink {
+        flavor: DrinkFlavors::Lemon,
+        fl_oz: 25.50
+    };
+    show_drink(my_drink)
+}
