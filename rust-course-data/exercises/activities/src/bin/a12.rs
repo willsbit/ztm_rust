866823273532c9aabb7d2df7a37fdb107b699ef8
Willian <willsbit@gmail.com>
2022-03-14 14:02:33 -0300

    Using impl, Self and &self

--- rust-course-data/exercises/activities/src/bin/a12.rs
+++ rust-course-data/exercises/activities/src/bin/a12.rs
@@ -9,5 +9,41 @@
 // * Use an enum for the box color
 // * Implement functionality on the box struct to create a new box
 // * Implement functionality on the box struct to print the characteristics
+struct Dimensions {
+    width: f64,
+    height: f64,
+    depth: f64,
+}
+#[derive(Debug)]
+enum Color {
+    Blue,
+    Yellow,
+    Red
+}
 
-fn main() {}
+struct Box {
+    dimensions: Dimensions,
+    weight: f64,
+    color: Color
+}
+
+impl Box {
+    fn create_box(weight:f64, color:Color, dimensions:Dimensions) -> Self {
+        Self {
+            dimensions,
+            weight,
+            color
+        }
+    }
+    fn print_characteristics(&self) {
+        println!("The box has dimensions (w-h-d): {:?} x {:?} x {:?}m",
+                 self.dimensions.width, self.dimensions.height, self.dimensions.depth);
+        println!("The box weighs {:?} kg", self.weight);
+        println!("The box is {:?}", self.color);
+    }
+
+}
+fn main() {
+    let my_box = Box::create_box(2.5, Color::Blue, Dimensions {width:2.5, height:3.0, depth:1.0});
+    my_box.print_characteristics()
+}
