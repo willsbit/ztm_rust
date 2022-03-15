5da20448e18dec76f4755f1b978be6eac4ae63b4
Willian <willsbit@gmail.com>
2022-03-15 14:05:58 -0300

    Enums with additional data and advanced match blocks

--- rust-course-data/exercises/activities/src/bin/a15.rs
+++ rust-course-data/exercises/activities/src/bin/a15.rs
@@ -10,5 +10,25 @@
 // * Use an enum for the tickets with data associated with each variant
 // * Create one of each ticket and place into a vector
 // * Use a match expression while iterating the vector to print the ticket info
+enum Tickets {
+    Backstage(String, f64),
+    Vip(String, f64),
+    Standard(f64)
+}
 
-fn main() {}
+
+fn main() {
+    let ticket_list = vec![Tickets::Backstage("Will".to_owned(), 30.00),
+                                      Tickets::Vip("Carol".to_owned(), 30.00),
+                                      Tickets::Standard(15.00)];
+
+    for ticket in ticket_list {
+        match ticket {
+            Tickets::Backstage(owner, price) =>
+                println!("The owner of this Backstage ticket is {:?} and it costs {:?}", owner, price),
+            Tickets::Vip(owner, price) =>
+                println!("The owner of this VIP ticket is {:?} and it costs {:?}", owner, price),
+            Tickets::Standard(price) => println!("This Standard ticket costs {:?}", price),
+        }
+    }
+}
