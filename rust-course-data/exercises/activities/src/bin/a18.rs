d45ccbc607e0f3ae6114e505f38ccb7d56e1aaca
Willian <willsbit@gmail.com>
2022-03-18 17:52:23 -0300

    Using Result<T, E>

--- rust-course-data/exercises/activities/src/bin/a18.rs
+++ rust-course-data/exercises/activities/src/bin/a18.rs
@@ -10,5 +10,28 @@
 // * Use a function to determine if a customer can make a restricted purchase
 // * Return a result from the function
 // * The Err variant should detail the reason why they cannot make a purchase
+#[derive(Debug)]
+struct Customer {
+    name: String,
+    age: i32
+}
 
-fn main() {}
+fn can_purchase(customer: &Customer) -> Result<bool, String> {
+    if customer.age < 21 {
+        Err("customer must be at least 21 years old".to_owned())
+    } else {
+        Ok(true)
+    }
+}
+
+fn main() {
+    let will = Customer {
+        name: "Will".to_owned(),
+        age: 25
+    };
+
+    let purchased = can_purchase(&will);
+    println!("{:?}", purchased);
+    // pick_choice(&will);
+
+}
