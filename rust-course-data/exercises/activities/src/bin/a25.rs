9052c1d9d7caa65344a9906c37c5a4f901fca107
Willian <willsbit@gmail.com>
2022-04-15 20:34:51 -0300

    Implementing custom traits.

--- rust-course-data/exercises/activities/src/bin/a25.rs
+++ rust-course-data/exercises/activities/src/bin/a25.rs
@@ -12,4 +12,46 @@
 // * Use a single function to print out the perimeter of the shapes
 //   * The function must utilize impl trait as a function parameter
 
-fn main() {}
+trait Perimeter {
+    fn calculate_perimeter(&self) -> i32;
+}
+
+struct Square {
+    side: i32
+}
+
+impl Perimeter for Square {
+    fn calculate_perimeter(&self) -> i32 {
+        let p = self.side * 4;
+        println!("The perimeter of the square is {:?}", p);
+        p
+    }
+}
+
+struct Triangle {
+    a: i32,
+    b: i32,
+    c: i32
+}
+
+impl Perimeter for Triangle {
+    fn calculate_perimeter(&self) -> i32 {
+        let p = self.a + self.b + self.c;
+        println!("The perimeter of the triangle is {:?}", p);
+        p
+    }
+}
+
+fn perimeter(geometry: impl Perimeter) {
+    Perimeter::calculate_perimeter(&geometry);
+}
+
+fn main() {
+    let sqr = Square {side: 30};
+    let triangle = Triangle {a: 3, b:4, c:5};
+
+    perimeter(sqr);
+    perimeter(triangle);
+}
+
+
