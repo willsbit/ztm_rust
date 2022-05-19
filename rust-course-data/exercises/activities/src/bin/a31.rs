f8a6e56da63db27cd0da57e92fa423cf538ac78b
Willian <willsbit@gmail.com>
2022-05-18 21:04:30 -0300

    Using trait objects, activity with Vec<Box<dyn T>>

--- rust-course-data/exercises/activities/src/bin/a31.rs
+++ rust-course-data/exercises/activities/src/bin/a31.rs
@@ -18,5 +18,42 @@
 // * Create trait objects and store them in a vector for processing
 // * Use a function to calculate the total cost
 // * Process at least 3 different materials
+trait Material {
+    fn cost(&self) -> f64;
+}
 
-fn main() {}
+struct Carpet(f64);
+impl Material for Carpet {
+    fn cost(&self) -> f64 {
+        self.0 * 10.0
+    }
+}
+
+struct Tile(f64);
+impl Material for Tile {
+    fn cost(&self) -> f64 {
+        self.0 * 15.0
+    }
+}
+
+struct Wood(f64);
+impl Material for Wood {
+    fn cost(&self) -> f64 {
+        self.0 * 20.0
+    }
+}
+
+fn total_cost(materials: &Vec<Box<dyn Material>>) -> f64 {
+    materials.iter().map(|sale| sale.cost()).sum()
+}
+
+fn main() {
+    let carpet_m2 = Box::new(Carpet(35.00));
+    let tile_m2 = Box::new(Tile(50.00));
+    let wood_m2 = Box::new(Wood(100.00));
+
+    let all_materials: Vec<Box<dyn Material>> = vec![carpet_m2, tile_m2, wood_m2];
+
+    let test_result = total_cost(&all_materials);
+    println!("The total cost is {:?}", test_result)
+}
