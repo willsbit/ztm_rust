2156cdd3e82dca46d966dd1bf472c38a6700a511
Willian <willsbit@gmail.com>
2022-05-19 22:17:23 -0300

    Applying compiler-enforced logic with generic typestates

--- rust-course-data/exercises/activities/src/bin/a34.rs
+++ rust-course-data/exercises/activities/src/bin/a34.rs
@@ -16,4 +16,63 @@
 // Notes:
 // * Optionally use generics for each state
 
-fn main() {}
+struct Luggage<State> {
+    id: u32,
+    state: State,
+}
+
+impl<State> Luggage<State> {
+    fn forward<NextState>(self, state:NextState) -> Luggage<NextState> {
+        Luggage {
+            id: self.id,
+            state: state,
+        }
+    }
+}
+
+struct CheckIn;
+
+impl Luggage<CheckIn> {
+    fn new(id:u32) -> Self {
+        Luggage {
+            id: id,
+            state: CheckIn,
+        }
+    }
+    fn forward_to_loading(self) -> Luggage<OnLoading> {
+        self.forward(OnLoading)
+    }
+}
+
+struct OnLoading;
+impl Luggage<OnLoading> {
+    fn forward_to_offloading(self) -> Luggage<OffLoading> {
+        self.forward(OffLoading)
+    }
+}
+struct OffLoading;
+impl Luggage<OffLoading> {
+    fn forward_to_pickup(self) -> Luggage<AwaitingPickup> {
+        self.forward(AwaitingPickup)
+    }
+}
+
+struct AwaitingPickup;
+impl Luggage<AwaitingPickup> {
+    fn forward_to_custody(self) -> Luggage<EndCustody> {
+        self.forward(EndCustody)
+    }
+}
+
+struct EndCustody;
+
+
+
+fn main() {
+    let luggage = Luggage::new(1);
+    let luggage_state = luggage.forward_to_loading().forward_to_offloading().forward_to_pickup().forward_to_custody();
+    match luggage_state.state {
+        EndCustody => println!("Luggage {} is in custody", luggage_state.id),
+        _ => println!("Luggage {} is not in custody", luggage_state.id),
+    }
+}
