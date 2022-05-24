c48b65ad8d5e79b8178778801df2f915e9c2e6aa
Willian <willsbit@gmail.com>
2022-05-24 17:06:47 -0300

    Shared ownership and interior mutability: Rc and RefCell

--- rust-course-data/exercises/activities/src/bin/a40.rs
+++ rust-course-data/exercises/activities/src/bin/a40.rs
@@ -20,9 +20,61 @@
 //   can both access the rental information
 // * Test your program by changing the vehicle status from both a storefront
 //   and from corporate
+use std::rc::Rc;
+use std::cell::RefCell;
+#[derive(Debug)]
+enum RentalStatus {
+    Available,
+    Unavailable,
+    Maintenance,
+    Rented,
+}
+#[derive(Debug)]
+struct Rental{
+    vehicle_type: String,
+    vin: String,
+    status: RentalStatus,
+}
 
-struct Corporate;
+type RentalPtr = Rc<RefCell<Rental>>;
 
-struct StoreFront;
+#[derive(Debug)]
+struct Corporate(RentalPtr);
+#[derive(Debug)]
+struct StoreFront(RentalPtr);
 
-fn main() {}
+fn main() {
+    let rental1 = Rc::new(RefCell::new(Rental {
+        vehicle_type: "Uno".to_string(),
+        vin: "12345".to_string(),
+        status: RentalStatus::Available,
+    }));
+
+    let corporate = Corporate(Rc::clone(&rental1));
+    let store_front = StoreFront(Rc::clone(&rental1));
+
+    let rental2 = Rc::new(RefCell::new(Rental {
+        vehicle_type: "MOBI".to_string(),
+        vin: "321654".to_string(),
+        status: RentalStatus::Available,
+    }));
+
+    let corporate2 = Corporate(Rc::clone(&rental2));
+    let store_front2 = StoreFront(Rc::clone(&rental2));
+
+    dbg!(corporate.0.borrow());
+    dbg!(store_front.0.borrow());
+
+    dbg!(corporate2.0.borrow());
+    dbg!(store_front2.0.borrow());
+
+    // Change the status of the vehicle
+    rental2.replace(Rental {
+        vehicle_type: "Strada".to_string(),
+        vin: "99999".to_string(),
+        status: RentalStatus::Rented,
+    });
+
+    dbg!(corporate2.0.borrow());
+    dbg!(store_front2.0.borrow());
+}
